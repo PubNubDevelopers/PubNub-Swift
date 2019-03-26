373eadee86f15be7a79345ddc1ce73dfb30eae29
Chandler Mayo <chandler@chandlermayo.com>
2019-03-26 16:09:12 -0700

    Renamed project and updated podfile.

--- PubNub Demo/PubSub.playground/Contents.swift
+++ PubNub Demo/PubSub.playground/Contents.swift
@@ -10,8 +10,8 @@ class PubNubPublisher: NSObject {
     
     required init(channel: String) {
         self.client = PubNub.clientWithConfiguration(PNConfiguration(
-            publishKey: "pub-c-782360a0-ace3-411a-9707-3dbcdc0b86a4",
-            subscribeKey: "sub-c-5eb10e66-f816-11e8-8ebf-6a684a5fb351"))
+            publishKey: "pub-c-9b32597c-37a8-4bee-b4b5-92c2f2f2a6eb",
+            subscribeKey: "sub-c-68fe425a-6e4e-11e8-a49b-66b3abd5adf6"))
         self.channel = channel
         super.init()
     }
